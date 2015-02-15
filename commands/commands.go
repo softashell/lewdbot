a27d968a28cb5dcbe261d8cc53a93507b0ec65dd
nixx quality <nixx@is-fantabulo.us>
2015-02-15 15:45:10 +0100

    splitting everything up a bit more

--- commands/commands.go
+++ commands/commands.go
@@ -8,6 +8,34 @@ import (
 	"strings"
 )
 
+func adminAdd(settings Settings, arg1 string) string {
+	if _, err := steamid.NewId(arg1); err != nil {
+		return "invalid steam id"
+	}
+
+	return fmt.Sprintf("added %s to admin list (but not really, that's not implemented)", arg1)
+}
+
+func blacklistAdd(settings Settings, arg1 string) string {
+	id, err := steamid.NewId(arg1)
+	if err != nil {
+		return "invalid group id"
+	}
+
+	settings.SetGroupBlacklisted(id, true)
+	return fmt.Sprintf("added %s to group blacklist", arg1)
+}
+
+func blacklistRemove(settings Settings, arg1 string) string {
+	id, err := steamid.NewId(arg1)
+	if err != nil {
+		return "invalid group id"
+	}
+
+	settings.SetGroupBlacklisted(id, false)
+	return fmt.Sprintf("removed %s from group blacklist", arg1)
+}
+
 func Handle(message string, settings Settings) (bool, string) {
 	if !strings.HasPrefix(message, "!") {
 		return false, ""
@@ -23,11 +51,7 @@ func Handle(message string, settings Settings) (bool, string) {
 			return true, "not enough arguments"
 		}
 
-		if _, err := steamid.NewId(arg[1]); err != nil {
-			return true, "ERROR: invalid steam id"
-		}
-
-		return true, fmt.Sprintf("added %s to admin list (but not really, that's not implemented)", arg[1])
+		return true, adminAdd(settings, arg[1])
 
 	case "blacklist.add":
 		arg := regex.BlacklistAddArguments.FindStringSubmatch(message)
@@ -36,13 +60,7 @@ func Handle(message string, settings Settings) (bool, string) {
 			return true, "not enough arguments"
 		}
 
-		id, err := steamid.NewId(arg[1])
-		if err != nil {
-			return true, "ERROR: invalid group id"
-		}
-
-		settings.SetGroupBlacklisted(id, true)
-		return true, fmt.Sprintf("adding %s to group blacklist", arg[1])
+		return true, blacklistAdd(settings, arg[1])
 
 	case "blacklist.remove":
 		arg := regex.BlacklistRemoveArguments.FindStringSubmatch(message)
@@ -51,17 +69,9 @@ func Handle(message string, settings Settings) (bool, string) {
 			return true, "not enough arguments"
 		}
 
-		id, err := steamid.NewId(arg[1])
-		if err != nil {
-			return true, "ERROR: invalid group id"
-		}
-
-		settings.SetGroupBlacklisted(id, false)
-		return true, fmt.Sprintf("removing %s from group blacklist", arg[1])
+		return true, blacklistRemove(settings, arg[1])
 
 	default:
 		return true, fmt.Sprintf("unknown command: %s", command)
 	}
-
-	return true, ""
 }
