928294e99efac64702cc47ddb71194d4e56a3257
nixx quality <nixx@is-fantabulo.us>
2015-02-15 15:37:51 +0100

    moved command handling to separate file

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/Philipp15b/go-steam/socialcache"
 	"github.com/Philipp15b/go-steam/steamid"
 	cobe "github.com/pteichman/go.cobe"
+	"github.com/softashell/lewdbot/commands"
 	"github.com/softashell/lewdbot/regex"
 	. "github.com/softashell/lewdbot/settings"
 	"io/ioutil"
@@ -107,74 +108,13 @@ func IsMaster(master steamid.SteamId) bool {
 	return false
 }
 
-func ObeyMaster(client *steam.Client, master steamid.SteamId, message string) (bool, string) {
-	if !strings.HasPrefix(message, "!") {
-		return false, ""
-	}
-
-	log.Printf("Obeying master %s %s", master, message)
-
-	command := regex.CommandName.FindStringSubmatch(message)[1]
-
-	switch command {
-	case "admin.add":
-		arg := regex.AdminAddArguments.FindStringSubmatch(message)
-
-		if len(arg) < 1 {
-			return true, "not enough arguments"
-		}
-
-		if _, err := steamid.NewId(arg[1]); err != nil {
-			return true, "ERROR: invalid steam id"
-		}
-
-		return true, fmt.Sprintf("added %s to admin list (but not really, that's not implemented)", arg[1])
-
-	case "blacklist.add":
-		arg := regex.BlacklistAddArguments.FindStringSubmatch(message)
-
-		if len(arg) < 1 {
-			return true, "not enough arguments"
-		}
-
-		id, err := steamid.NewId(arg[1])
-		if err != nil {
-			return true, "ERROR: invalid group id"
-		}
-
-		settings.SetGroupBlacklisted(id, true)
-		return true, fmt.Sprintf("adding %s to group blacklist", arg[1])
-
-	case "blacklist.remove":
-		arg := regex.BlacklistRemoveArguments.FindStringSubmatch(message)
-
-		if len(arg) < 1 {
-			return true, "not enough arguments"
-		}
-
-		id, err := steamid.NewId(arg[1])
-		if err != nil {
-			return true, "ERROR: invalid group id"
-		}
-
-		settings.SetGroupBlacklisted(id, false)
-		return true, fmt.Sprintf("removing %s from group blacklist", arg[1])
-
-	default:
-		return true, fmt.Sprintf("unknown command: %s", command)
-	}
-
-	return true, ""
-}
-
 func ReplyToMessage(client *steam.Client, e *steam.ChatMsgEvent) {
 	if !e.IsMessage() {
 		return
 	}
 
 	if IsMaster(e.ChatterId) {
-		if master, reply := ObeyMaster(client, e.ChatterId, e.Message); master == true {
-			// Command executed, no need to reply
+		if master, reply := commands.Handle(client, e.ChatterId, e.Message, settings); master == true {
 			client.Social.SendMessage(e.ChatterId, steamlang.EChatEntryType_ChatMsg, reply)
 			return
 		}
