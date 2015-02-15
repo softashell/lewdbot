0ed8112afe44c4778bc0917a614ee9a968298ed6
soft as HELL <softashell@cock.li>
2015-02-15 07:29:22 +0200

    Remove ' from regex

--- main.go
+++ main.go
@@ -95,7 +95,7 @@ func GenerateReply(client *steam.Client, steamid steamid.SteamId, message string
 	reply = strings.TrimSpace(reply)
 
 	reply = strings.Replace(reply, "lewdbot", GetName(client, steamid), -1)
-	reply = regexp.MustCompile(`[\.—-]+$`).ReplaceAllString(reply, "~")
+	reply = regexp.MustCompile(`[\.,—-]+$`).ReplaceAllString(reply, "~")
 
 	// TODO: Stop the cancer
 	lewdbrain.Learn(message)
